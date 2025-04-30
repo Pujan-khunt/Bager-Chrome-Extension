2f409a0658b038c0e7d0966cd43f0a2154b3f005
Pujan-khunt <pujankhunt2412@gmail.com>
2025-04-30 15:02:45 +0530

    feat: implement popup ui template and navbar frontend

---
+++ src/popup/Popup.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "./parts/Footer";
+import Main from "./parts/Main";
+import Navbar from "./parts/Navbar";
+import { Separator } from "@/components/ui/separator";
+
+const Popup: React.FC = () => {
+  return (
+    <div className="w-[380px] h-[600px] bg-black">
+      <Navbar />
+      <Separator/>
+      <Main />
+      <Footer />
+    </div>
+  );
+};
+
+export default Popup;
---
+++ src/popup/parts/Navbar.tsx
@@ -0,0 +1,26 @@
+import { PlusIcon, SettingsIcon, SunIcon } from "lucide-react";
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="w-full bg-[#1a1a1a] h-16 flex px-4 items-center">
+      {/* "Bager" Text - Left */}
+      <p className="grow text-white font-extrabold text-2xl">Bager</p>
+
+      {/* Utility Buttons - Right */}
+      <div className="flex gap-x-4 items-center text-white">
+        <div className="p-1 border-2 border-transparent hover:border-blue-600 hover:rounded-md box-border transition-all duration-150">
+          <PlusIcon cursor="pointer" />
+        </div>
+        <div className="p-1 border-2 border-transparent hover:border-yellow-400 hover:rounded-md box-border transition-all duration-150">
+          <SunIcon cursor="pointer" />
+        </div>
+        <div className="p-1 border-2 border-transparent hover:border-gray-400 hover:rounded-md box-border transition-all duration-150">
+          <SettingsIcon cursor="pointer" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
