8905851f6b4cb025ca5f7e8c4d40d7df74a3c212
Pujan-khunt <pujankhunt2412@gmail.com>
2025-04-30 12:30:13 +0530

    config: configure vite.config.ts and manifest.json to correctly find input files and output them accordingly

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,30 @@ import path from 'node:path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: path.resolve(__dirname, "popup.html"),
+        background: path.resolve(__dirname, "src/background/background.ts"),
+        content: path.resolve(__dirname, "src/content/content.tsx")
+      },
+      output: {
+        // entryFileNames: "assets/[name].js",
+        entryFileNames: ({ name }) => {
+          if (name === "background") {
+            return "assets/background/background.js";
+          }
+          else if (name === "content") {
+            return "assets/content/content.js";
+          }
+          return "assets/[name].js";
+        }
+      }
+    },
+    outDir: "dist",
+    sourcemap: true, // Only for Development, false in production
+    emptyOutDir: true
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src")
